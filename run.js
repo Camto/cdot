5af11abee96507d7a850fb0cc921405f93dbc2c6
Benjamin Philippe Applegate <www.ben.pr@gmail.com>
2021-10-10 02:34:37 -0500

    Make nested pipelines and `?`s work.

--- run.js
+++ run.js
@@ -2,37 +2,52 @@
 
 let std = require("./std");
 
-function run(pipeline, pipe_vals) {
+function run(pipeline, pipe_vals = [], qs = []) {
 	return pipeline.reduce(
-		(pipe_vals, pipe_section) => run_pipe_section(pipe_section, pipe_vals),
+		(pipe_vals, pipe_section) => run_pipe_section(pipe_section, pipe_vals, qs),
 		pipe_vals
 	);
 }
 
-function run_pipe_section(pipe_section, pipe_vals) {
+function run_pipe_section(pipe_section, pipe_vals, qs) {
 	switch(pipe_section.type) {
 		case "call":
-			let args = pipe_section.args.map(run_expr);
+			let args = pipe_section.args.map(expr => run_expr(
+				expr,
+				pipe_section.sets_qs
+					? pipe_vals
+					: qs
+			));
 			
+			let func;
 			if(pipe_section.i == 0)
-				return std.built_ins[pipe_section.func](args);
+				func = std.built_ins[pipe_section.func];
 			else
 				throw "User functions uninplemented";
+			
+			return func(
+				pipe_section.sets_qs
+					? args
+					: args.concat(pipe_vals));
 		default:
 			throw "Pipe type unimplemented";
 	}
 }
 
-function run_expr(expr) {
+function run_expr(expr, qs) {
 	switch(expr.type) {
+		case "expr":
+			return run(expr.data, [], qs)[0]; // Add check
 		case "num":
 			return expr;
+		case "?":
+			return qs[expr.n - 1]; // Add check
 		default:
 			let op = std.ops[expr.type];
 			if(expr.left)
-				return op(run_expr(expr.left), run_expr(expr.right));
+				return op(run_expr(expr.left, qs), run_expr(expr.right, qs));
 			else
-				return op(run_expr(expr.arg));
+				return op(run_expr(expr.arg, qs));
 	}
 }
 
